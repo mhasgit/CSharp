2e70d74f8a3d03289535f5fa4c92f43aca40a633
Ashraf Ali <ashrafxcoder@users.noreply.github.com>
2023-02-18 11:08:31 +0000

    added stream hierarchy classes

--- InputOutput/InputOutputStreams/InputOutputStreams/Program.cs
+++ InputOutput/InputOutputStreams/InputOutputStreams/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InputOutputStreams
+﻿namespace InputOutputStreams
 {
-    internal class Program
+    using System;
+    using System.CodeDom;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    public class Program
     {
         static void Main(string[] args)
         {
+            // Byte-oriented IO
+            // Character-oriented IO
+
+            Stream stream = null;
+            FileStream fileStream = null;
+            MemoryStream memoryStream = new MemoryStream();
+            BufferedStream bufferedStream = null;
+
+            byte[] memoryBuffer = new byte[1024];
+            MemoryStream mStream = new MemoryStream();
+
+            TextReader textReader = null;
+
+            StreamReader streamReader = null;
+            StreamWriter streamWriter = null;
+
+            StringReader reader;
+            StringWriter writer;
         }
+    }
+
+    public class ByteArrayStream : MemoryStream
+    {
+        public ByteArrayStream() : base() { }
+
+        public ByteArrayStream(byte[] buffer) : base(buffer) { }
     }
 }
