c0d786ffde8c2379d177181a30855c6bfe9ff1a4
mhasgit <clumsy7674@gmail.com>
2023-08-22 18:58:01 +0500

    Networking

---
+++ Networking/Networking/DnsResolverForm.cs
@@ -0,0 +1,36 @@
+using System.Net;
+
+namespace Networking
+{
+    public partial class DnsResolverForm : Form
+    {
+        public DnsResolverForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLookup_Click(object sender, EventArgs e)
+        {
+            //Dns.GetHostByName(this)
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var hostName = textBox1.Text;
+            IPHostEntry hostEntry = Dns.GetHostByName(hostName);
+            this.textBox2.Text = hostEntry.AddressList[0].ToString();
+            //IPAddress.Loopback;
+
+        }
+    }
+}
---
+++ Networking/Networking/NetHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    internal class NetHelper
+    {
+        /*
+         * Class A   (byte 1) 1-126 approx 16M devices
+         * Class B   (byte 1) 128-191 approx 65535 devices
+         * Class C   (byte 1) 192-223 approx 254 devices
+         * Class D   (byte 1) 224-239 Multicast ip addresses
+         * Class E   (byte 1) 240-255 Uses for Testing
+         */
+
+        /*
+         * Private Ip addresses is for LAN
+         * 
+         * Class A private ip is 10
+         * Class B 172.16-172.31
+         * Class C 192.168.0-192.168.255
+         */
+    }
+}
