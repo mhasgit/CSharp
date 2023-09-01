7898dceba89c18cca937f86f6c57731d0b63a885
mhasgit <clumsy7674@gmail.com>
2023-09-01 19:13:41 +0500

    OOP and some advance concepts

--- DelegateAndLambdas/DelegateAndLambdas/Program.cs
+++ DelegateAndLambdas/DelegateAndLambdas/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Text;
@@ -20,11 +21,50 @@ namespace DelegateAndLambdas
             // MulticastDelegate();
             // TestDelToFuncMethod();
             // AnonymousDelegate();
-             TestGenericDel();
+            // TestGenericDel();
             // WorkingWithEvent();
             // TestBackAccount();
-
+            // TestPolymorphism();
+            // LinqMultipleCondition();
         }
+
+        #region Polymorphism
+
+
+        public static void TestPolymorphism()
+        {
+            //Animal animal = new Animal();
+            //animal.Speak();
+
+            Cat cat = new Cat();
+               cat.Speak();
+        }
+
+        public class Animal
+        {
+            public virtual void Speak()
+            {
+                Console.WriteLine("The animal makes a sound");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("The dog barks");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("The cat meows");
+            }
+        }
+        #endregion
+
         #region Delegate Basics
 
         private static void TestGenericDel()
@@ -123,6 +163,89 @@ namespace DelegateAndLambdas
 
         #endregion
 
+        #region Linq Basics
 
+        private static void LinqToObjectsQuery()
+        {
+            // Linq to objects query
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evenNumbers = from number in numbers
+                              where number % 2 == 0
+                              select number;
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            // With Lambda syntax
+
+            // var evenNum = numbers.Where(n => n % 2 == 0);
+        }
+
+        private static void LinqMultipleCondition()
+        {
+            var fruits = new List<string> { "date", "mango", "banana", "cherry", "apple" };
+
+            var results = from fruit in fruits
+                          where fruit.Length > 3
+                          orderby fruit
+                          select fruit;
+
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+
+        #endregion
+
+        #region Built-in data structures
+
+        private static void WorkingWithDataStructures()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(numbers[2]);
+
+            
+            List<string> fruits = new List<string> { "apple", "banana", "cherry" };
+            fruits.Add("date");
+            Console.WriteLine(fruits[2]);
+
+            Dictionary<string, int> ages = new Dictionary<string, int>
+            {
+                {"Alice", 25},
+                {"Bob", 30}
+            };
+
+            ages["Charlie"] = 28;
+            Console.WriteLine(ages["Bob"]);
+
+            Queue<string> names = new Queue<string>();
+            names.Enqueue("Alice");
+            names.Enqueue("Bob");
+            Console.WriteLine(names.Dequeue());
+
+            Stack<int> nums = new Stack<int>();
+            nums.Push(1);
+            nums.Push(2);
+            Console.WriteLine(nums.Pop());
+
+            HashSet<int> uniqueNumbers = new HashSet<int> { 1, 2, 3, 2, 1 };
+            Console.WriteLine(uniqueNumbers.Count);
+
+            LinkedList<string> days = new LinkedList<string>();
+            days.AddLast("Monday");
+            days.AddLast("Tuesday");
+            days.AddFirst("Sunday");
+            Console.WriteLine(days.First.Value);
+
+            (var name, var age, var country) = ("Alice", 25, "USA");
+            Console.WriteLine($"Name: {name}, Age: {age}, Country: {country}");
+        }
+
+
+
+        #endregion
     }
 }
